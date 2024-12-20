d1a12ac7779025b99553382afcc0e81bf278f920
Matt Hunzinger <matthunz2@gmail.com>
2024-12-20 16:42:30 -0500

    feat: create Entity newtype

--- src/Data/Aztecs.hs
+++ src/Data/Aztecs.hs
@@ -12,11 +12,15 @@ import Data.Dynamic (Dynamic, fromDynamic, toDyn)
 import Data.Map
 import Data.Typeable
 
-table' :: [a] -> Storage a
+newtype Entity = Entity Int deriving (Show)
+
+data EntityComponent a = EntityComponent Entity a
+
+table' :: [EntityComponent a] -> Storage a
 table' cs =
   Storage
     { empty' = table' [],
-      spawn' = \a -> table' (a : cs)
+      spawn' = \e a -> table' (EntityComponent e a : cs)
     }
 
 table :: Storage a
@@ -24,26 +28,27 @@ table = table' []
 
 data Storage a = Storage
   { empty' :: Storage a,
-    spawn' :: a -> Storage a
+    spawn' :: Entity -> a -> Storage a
   }
 
 class Component a where
   storage :: Storage a
   storage = table
 
-data World = World (Map TypeRep Dynamic) deriving (Show)
+data World = World (Map TypeRep Dynamic) Entity deriving (Show)
 
 newWorld :: World
-newWorld = World empty
+newWorld = World empty (Entity 0)
 
 spawn :: (Component a, Typeable a) => a -> World -> World
-spawn a (World w) =
+spawn a (World w (Entity e)) =
   World
     ( alter
         ( \maybeRow -> case maybeRow of
-            Just row -> fmap (\row' -> toDyn $ spawn' row' a) (fromDynamic row)
-            Nothing -> Just $ toDyn $ spawn' storage a
+            Just row -> fmap (\row' -> toDyn $ spawn' row' (Entity e) a) (fromDynamic row)
+            Nothing -> Just $ toDyn $ spawn' storage (Entity e) a
         )
         (typeOf a)
         w
     )
+    (Entity (e + 1))
