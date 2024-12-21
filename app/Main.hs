b19cfc923b024ccd4d17eb160d9e2c4b3fbbb5d8
Matt Hunzinger <matthunz2@gmail.com>
2024-12-20 19:40:59 -0500

    feat: impl queries

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,20 @@
 module Main where
 
 import Data.Aztecs
+import qualified Data.Aztecs as ECS
 
 data X = X Int deriving (Show)
 
 instance Component X
 
+data Y = Y Int deriving (Show)
+
+instance Component Y
+
+data Q = Q X Y deriving (Show)
+
 main :: IO ()
 main =
   let (e, w) = spawn (X 1) newWorld
-   in print $ get @X e w
+      w' = insert e (Y 2) w
+   in print $ runQuery (Q <$> ECS.read <*> ECS.read) w'
--- src/Data/Aztecs.hs
+++ src/Data/Aztecs.hs
@@ -1,23 +1,33 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Data.Aztecs
-  ( Storage (..),
+  ( Entity,
+    EntityComponent (..),
+    Storage (..),
     table,
     Component (..),
     World,
     newWorld,
     spawn,
+    insert,
     get,
+    Query,
+    read,
+    runQuery,
   )
 where
 
 import Data.Dynamic (Dynamic, fromDynamic, toDyn)
 import Data.Functor ((<&>))
 import Data.List (find)
-import Data.Map
+import Data.Map (Map, alter, empty, lookup)
+import Data.Maybe (fromMaybe, isJust)
 import Data.Typeable
+import Prelude hiding (read)
 
 newtype Entity = Entity Int deriving (Eq, Show)
 
-data EntityComponent a = EntityComponent Entity a
+data EntityComponent a = EntityComponent Entity a deriving (Show)
 
 table' :: [EntityComponent a] -> Storage a
 table' cs =
@@ -26,7 +36,8 @@ table' cs =
       spawn' = \e a -> table' (EntityComponent e a : cs),
       get' = \e ->
         find (\(EntityComponent e' _) -> e == e') cs
-          <&> \(EntityComponent _ a) -> a
+          <&> \(EntityComponent _ a) -> a,
+      toList = cs
     }
 
 table :: Storage a
@@ -35,7 +46,8 @@ table = table' []
 data Storage a = Storage
   { empty' :: Storage a,
     spawn' :: Entity -> a -> Storage a,
-    get' :: Entity -> Maybe a
+    get' :: Entity -> Maybe a,
+    toList :: [EntityComponent a]
   }
 
 class Component a where
@@ -65,8 +77,64 @@ spawn = f (Proxy)
           (Entity (e + 1))
       )
 
+insert :: (Component c, Typeable c) => Entity -> c -> World -> World
+insert = f Proxy
+  where
+    f :: (Component c, Typeable c) => Proxy c -> Entity -> c -> World -> World
+    f p e c (World w e') =
+      World
+        ( alter
+            ( \maybeRow -> Just $ toDyn $ g (maybeRow >>= fromDynamic) e c
+            )
+            (typeOf p)
+            w
+        )
+        e'
+    g :: (Component c, Typeable c) => Maybe (Storage c) -> Entity -> c -> Storage c
+    g maybeRow e c = case maybeRow of
+      Just row -> spawn' row e c
+      Nothing -> spawn' storage e c
+
+getRow :: (Typeable c) => Proxy c -> World -> Maybe (Storage c)
+getRow p (World w _) = Data.Map.lookup (typeOf p) w >>= fromDynamic
+
 get :: (Typeable c) => Entity -> World -> Maybe c
 get e (World w _) = f (Proxy)
   where
     f :: (Typeable c) => Proxy c -> Maybe c
     f p = Data.Map.lookup (typeOf p) w >>= fromDynamic >>= flip get' e
+
+data ReadWrites = ReadWrites [TypeRep] [TypeRep]
+
+instance Semigroup ReadWrites where
+  ReadWrites rs ws <> ReadWrites rs' ws' = ReadWrites (rs <> rs') (ws <> ws')
+
+instance Monoid ReadWrites where
+  mempty = ReadWrites [] []
+
+data Query a = Query ReadWrites (Maybe [Entity] -> World -> [a])
+  deriving (Functor)
+
+instance Applicative Query where
+  pure a = Query mempty (\_ _ -> [a])
+  Query rs f <*> Query rs' f' = Query (rs <> rs') (\es w -> f es w <*> f' es w)
+
+read :: (Typeable a) => Query a
+read = f Proxy
+  where
+    f :: (Typeable a) => Proxy a -> Query a
+    f p =
+      Query
+        (ReadWrites [typeOf p] [])
+        ( \es w ->
+            let x = (fromMaybe [] (fmap toList (getRow p w)))
+             in case es of
+                  Just es' ->
+                    map
+                      (\(EntityComponent _ a) -> a)
+                      (filter (\(EntityComponent e _) -> isJust $ find (== e) es') x)
+                  Nothing -> map (\(EntityComponent _ a) -> a) x
+        )
+
+runQuery :: Query a -> World -> [a]
+runQuery (Query _ f) w = f Nothing w
