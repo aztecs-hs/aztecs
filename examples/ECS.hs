8f7e81b74f372e069ab8ae072343dabf585cdb90
Matt Hunzinger <matthunz2@gmail.com>
2025-01-09 20:55:13 -0500

    feat: add spawn fn

--- examples/ECS.hs
+++ examples/ECS.hs
@@ -6,6 +6,6 @@ import qualified Data.Aztecs as W
 
 main :: IO ()
 main = do
-  w <- W.insert (W.Entity 0) (W.ComponentID 0) (0 :: Int) W.empty
-  x <- W.lookup @Int (W.Entity 0) (W.ComponentID 0) w
+  (e, w) <- W.spawn (W.ComponentID 0) (0 :: Int) W.empty
+  x <- W.lookup @Int e (W.ComponentID 0) w
   print x
--- src/Data/Aztecs.hs
+++ src/Data/Aztecs.hs
@@ -6,6 +6,7 @@ module Data.Aztecs
     EntityRecord (..),
     World (..),
     empty,
+    spawn,
     insert,
     lookup,
   )
@@ -39,30 +40,40 @@ data World = World
     nextArchetypeId :: ArchetypeID,
     componentIds :: Map ComponentID (Map ArchetypeID ColumnID),
     nextComponentId :: ComponentID,
-    entities :: Map Entity EntityRecord
+    entities :: Map Entity EntityRecord,
+    nextEntity :: Entity
   }
 
 empty :: World
-empty = World Map.empty (ArchetypeID 0) Map.empty (ComponentID 0) Map.empty
+empty = World Map.empty (ArchetypeID 0) Map.empty (ComponentID 0) Map.empty (Entity 0)
+
+spawn :: (Typeable c) => ComponentID -> c -> World -> IO (Entity, World)
+spawn cId c w = do
+  let e = nextEntity w
+  w' <- insertNew e cId c (w {nextEntity = Entity (unEntity e + 1)})
+  return (e, w')
 
 insert :: (Typeable c) => Entity -> ComponentID -> c -> World -> IO World
 insert e cId c w = case Map.lookup e (entities w) of
   Just record -> error "TODO"
-  Nothing -> case Map.lookup cId (componentIds w) of
-    Just colIds -> error "TODO"
-    Nothing -> do
-      let archId = nextArchetypeId w
-      table <- Table.replicate 1 c
-      let archetypes' = Map.insert archId (Archetype table) (archetypes w)
-          componentIds' = Map.insert cId (Map.singleton archId (ColumnID 0)) (componentIds w)
-          entities' = Map.insert e (EntityRecord archId (TableID 0)) (entities w)
-      return
-        w
-          { archetypes = archetypes',
-            componentIds = componentIds',
-            entities = entities',
-            nextArchetypeId = ArchetypeID (unArchetypeId archId + 1)
-          }
+  Nothing -> insertNew e cId c w
+
+insertNew :: (Typeable c) => Entity -> ComponentID -> c -> World -> IO World
+insertNew e cId c w = case Map.lookup cId (componentIds w) of
+  Just colIds -> error "TODO"
+  Nothing -> do
+    let archId = nextArchetypeId w
+    table <- Table.replicate 1 c
+    let archetypes' = Map.insert archId (Archetype table) (archetypes w)
+        componentIds' = Map.insert cId (Map.singleton archId (ColumnID 0)) (componentIds w)
+        entities' = Map.insert e (EntityRecord archId (TableID 0)) (entities w)
+    return
+      w
+        { archetypes = archetypes',
+          componentIds = componentIds',
+          entities = entities',
+          nextArchetypeId = ArchetypeID (unArchetypeId archId + 1)
+        }
 
 lookup :: (Typeable c) => Entity -> ComponentID -> World -> IO (Maybe c)
 lookup e cId w = case Map.lookup e (entities w) of
