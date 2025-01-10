573e77effee4861e99721c2c5d5a229a6ff77ae2
Matt Hunzinger <matthunz2@gmail.com>
2025-01-10 03:06:10 -0500

    test: add tests for queries

--- test/Main.hs
+++ test/Main.hs
@@ -2,5 +2,26 @@
 
 module Main (main) where
 
+import qualified Data.Aztecs as W
+import qualified Data.Aztecs.Query as Q
+import Test.Hspec
+
+newtype X = X Int deriving (Eq, Show)
+
+newtype Y = Y Int deriving (Eq, Show)
+
 main :: IO ()
 main = hspec $ do
+  describe "Data.Aztecs.Query.all" $ do
+    it "queries multiple components" $ do
+      let (_, w) = W.spawn (X 0) W.empty
+          (_, w') = W.spawn (X 1) w
+          (x, _) = Q.all (Q.read) w'
+      x `shouldMatchList` [X 0, X 1]
+    it "queries multiple components" $ do
+      let (e, w) = W.spawn (X 0) W.empty
+          w' = W.insert e (Y 0) w
+          (e', w'') = W.spawn (X 1) w'
+          w''' = W.insert e' (Y 1) w''
+          (x, _) = Q.all ((,) <$> Q.read <*> Q.read) w'''
+      x `shouldMatchList` [(X 0, Y 0), (X 1, Y 1)]
