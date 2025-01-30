bc093e991b735ff1def13f3d88970ad59351f541
Matt Hunzinger <matthunz2@gmail.com>
2025-01-29 22:55:03 -0500

    feat: return accessed ComponentIds from systems before running

--- src/Data/Aztecs/System.hs
+++ src/Data/Aztecs/System.hs
@@ -9,32 +9,42 @@
 module Data.Aztecs.System where
 
 import Data.Aztecs.Access (Access, runAccess)
-import Data.Aztecs.Entity (ComponentIds, Entity, EntityT)
+import Data.Aztecs.Entity (ComponentIds (componentIds), Entity, EntityT)
 import Data.Aztecs.Query (IsEq, Queryable)
 import qualified Data.Aztecs.Query as Q
-import Data.Aztecs.View (View)
+import Data.Aztecs.View (View (..))
 import qualified Data.Aztecs.View as V
 import Data.Aztecs.World (World (..))
-import Data.Aztecs.World.Components (Components)
+import Data.Aztecs.World.Components (ComponentID, Components)
+import Data.Set (Set)
 
 class System m a where
   task :: Task m () ()
 
 runSystem :: forall m s. (System m s, Monad m) => World -> m World
 runSystem w = do
-  let (w', f) = runTask (task @m @s) w
+  let (w', _, f) = runTask (task @m @s) w
   (_, g, access) <- f () (components w')
   (_, w'') <- runAccess access w'
   return $ g w''
 
-newtype Task m i o = Task {runTask :: World -> (World, i -> Components -> m (o, World -> World, Access m ()))}
+-- | System task.
+newtype Task m i o = Task
+  { runTask ::
+      World ->
+      ( World,
+        [Set ComponentID],
+        i -> Components -> m (o, World -> World, Access m ())
+      )
+  }
 
 (<&>) :: (Monad m) => Task m i o -> Task m o a -> Task m i a
 t1 <&> t2 =
   Task $ \w ->
-    let (w', f) = runTask t1 w
-        (w'', g) = runTask t2 w'
+    let (w', cIds, f) = runTask t1 w
+        (w'', cIds', g) = runTask t2 w'
      in ( w'',
+          cIds <> cIds',
           \i cs -> do
             (o, f', access) <- f i cs
             (a, g', access') <- g o cs
@@ -42,7 +52,7 @@ t1 <&> t2 =
         )
 
 all :: forall m v. (Monad m, ComponentIds v, Queryable v) => Task m () [Entity v]
-all = view @v (\v cs -> pure $ V.queryAll v cs)
+all = view @_ @v (\v cs -> pure $ V.queryAll v cs)
 
 map ::
   forall m i o.
@@ -56,28 +66,32 @@ map ::
 map f = mapView (\v cs -> pure $ V.map f v cs)
 
 view ::
-  forall v m a.
-  (Monad m, ComponentIds v, Queryable v, Functor m) =>
+  forall m v a.
+  (Monad m, ComponentIds v, Queryable v) =>
   (View v -> Components -> m a) ->
   Task m () a
-view f = Task $ \w -> let (v, w') = V.view @v w in (w', \_ cs -> (,const w',pure ()) <$> f v cs)
+view f = Task $ \w ->
+  let (cIds, cs') = componentIds @v (components w')
+      (v, w') = V.view @v w {components = cs'}
+   in (w', [cIds], \_ cs -> (,const w',pure ()) <$> f v cs)
 
 mapView ::
-  forall v m a.
+  forall m v a.
   (Monad m, ComponentIds v, Queryable v) =>
   (View v -> Components -> m (a, View v)) ->
   Task m () a
 mapView f = Task $ \w ->
-  let (v, w') = V.view @v w
-   in (w', \_ cs -> (\(a, v') -> (a, const $ V.unview v' w', pure ())) <$> f v cs)
+  let (cIds, cs') = componentIds @v (components w')
+      (v, w') = V.view @v w {components = cs'}
+   in (w', [cIds], \_ cs -> (\(a, v') -> (a, const $ V.unview v' w', pure ())) <$> f v cs)
 
 -- | Queue an `Access` to alter the world after this task is complete.
 queue :: (Monad m) => Access m () -> Task m () ()
-queue a = Task (,\_ _ -> pure ((), id, a))
+queue a = Task (,[],\_ _ -> pure ((), id, a))
 
 run :: (Monad m) => (i -> m o) -> Task m i o
 run f =
   Task
-    (,\i _ -> do
-        o <- f i
-        return (o, id, pure ()))
+    (,[],\i _ -> do
+           o <- f i
+           return (o, id, pure ()))
