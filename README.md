be58ef8dffb5570ab8c42762aed5a2dcb1c0e83c
Matt Hunzinger <matthunz2@gmail.com>
2025-02-02 13:30:12 -0500

    docs: update SDL example

--- README.md
+++ README.md
@@ -44,6 +44,40 @@ main :: IO ()
 main = run app
 ```
 
+## SDL
+```hs
+import Control.Arrow ((>>>))
+import Data.Aztecs
+import qualified Data.Aztecs.Access as A
+import Data.Aztecs.Asset (load)
+import Data.Aztecs.SDL (Image (..), Window (..), sdlPlugin)
+import qualified Data.Aztecs.System as S
+import Data.Aztecs.Transform (Transform (..), transform)
+import SDL (V2 (..))
+
+data Setup
+
+instance System IO Setup where
+  task =
+    S.mapSingleWith (\assetServer -> load "example.png" assetServer)
+      >>> S.queueWith
+        ( \(texture, _) -> do
+            A.spawn_ Window {windowTitle = "Aztecs"}
+            A.spawn_ $
+              Image {imageTexture = texture, imageSize = V2 100 100}
+                :& transform {transformPosition = V2 100 100}
+            A.spawn_ $
+              Image {imageTexture = texture, imageSize = V2 200 200}
+                :& transform {transformPosition = V2 500 100}
+        )
+
+app :: Scheduler IO
+app = sdlPlugin <> schedule @_ @Startup @Setup []
+
+main :: IO ()
+main = run app
+```
+
 ## Benchmarks
 
 Aztecs is currently faster than [bevy-ecs](https://github.com/bevyengine/bevy/), a popular and high-performance ECS written in Rust, for simple mutating queries.
--- packages/aztecs-sdl/examples/Window.hs
+++ packages/aztecs-sdl/examples/Window.hs
@@ -9,26 +9,22 @@ import qualified Data.Aztecs.Access as A
 import Data.Aztecs.Asset (load)
 import Data.Aztecs.SDL (Image (..), Window (..), sdlPlugin)
 import qualified Data.Aztecs.System as S
-import Data.Aztecs.Transform (Transform (transformPosition), transform)
+import Data.Aztecs.Transform (Transform (..), transform)
 import SDL (V2 (..))
 
 data Setup
 
 instance System IO Setup where
   task =
-    S.mapSingleWith
-      ( \assetServer -> do
-          (assetId, assetServer') <- load "example.png" assetServer
-          return (assetId, assetServer')
-      )
+    S.mapSingleWith (\assetServer -> load "example.png" assetServer)
       >>> S.queueWith
-        ( \(assetId, _) -> do
-            A.spawn_ (Window {windowTitle = "Aztecs"})
+        ( \(texture, _) -> do
+            A.spawn_ Window {windowTitle = "Aztecs"}
             A.spawn_ $
-              Image {imageTexture = assetId, imageSize = V2 100 100}
+              Image {imageTexture = texture, imageSize = V2 100 100}
                 :& transform {transformPosition = V2 100 100}
             A.spawn_ $
-              Image {imageTexture = assetId, imageSize = V2 200 200}
+              Image {imageTexture = texture, imageSize = V2 200 200}
                 :& transform {transformPosition = V2 500 100}
         )
 
