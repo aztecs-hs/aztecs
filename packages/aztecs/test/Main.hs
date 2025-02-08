1848bb05296f6baa98240f2277137c2e5e09ae37
Matt Hunzinger <matthunz2@gmail.com>
2025-02-08 00:58:06 -0500

    test: setup QuickCheck

--- packages/aztecs/test/Main.hs
+++ packages/aztecs/test/Main.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main (main) where
 
 import Control.Arrow ((&&&))
 import Data.Aztecs
 import qualified Data.Aztecs.Query as Q
 import qualified Data.Aztecs.World as W
+import Data.Functor.Identity (Identity (..))
 import Test.Hspec
+import Test.QuickCheck
 
-newtype X = X Int deriving (Eq, Show)
+newtype X = X Int deriving (Eq, Show, Arbitrary)
 
 instance Component X
 
@@ -21,13 +25,15 @@ instance Component Z
 main :: IO ()
 main = hspec $ do
   describe "Data.Aztecs.Query.all" $ do
-    it "queries multiple components" $ do
-      let (_, w) = W.spawn (bundle $ X 0) W.empty
-          (_, w') = W.spawn (bundle $ X 1) w
-      (xs, _) <- Q.all (Q.fetch) w'
-      xs `shouldMatchList` [X 0, X 1]
+    it "queries multiple components" $ property prop_queryMultipleComponents
     it "queries a group of components" $ do
       let (e, w) = W.spawn (bundle $ X 0) W.empty
           w' = W.insert e (Y 1) w
       (xs, _) <- Q.all (Q.fetch &&& Q.fetch) w'
       xs `shouldMatchList` [(X 0, Y 1)]
+
+prop_queryMultipleComponents :: [X] -> Expectation
+prop_queryMultipleComponents xs =
+  let w = foldr (\x -> snd . W.spawn (bundle x)) W.empty xs
+      (queriedXs, _) = runIdentity $ Q.all Q.fetch w
+   in queriedXs `shouldMatchList` xs
