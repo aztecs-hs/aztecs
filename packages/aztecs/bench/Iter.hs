bfc8930cf1d70eef07c9b012959c706beb52ac44
Matt Hunzinger <matthunz2@gmail.com>
2025-02-13 13:36:56 -0500

    test: update bench

--- packages/aztecs/bench/Iter.hs
+++ packages/aztecs/bench/Iter.hs
@@ -1,10 +1,10 @@
+{-# LANGUAGE Arrows #-}
 {-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 
 import Criterion.Main
 import Data.Aztecs
 import qualified Data.Aztecs.Query as Q
-import Data.Aztecs.World (World)
+import qualified Data.Aztecs.System as S
 import qualified Data.Aztecs.World as W
 
 newtype Position = Position Int deriving (Show)
@@ -15,17 +15,27 @@ newtype Velocity = Velocity Int deriving (Show)
 
 instance Component Velocity
 
-run :: World -> World
-run w = let !(_, w') = Q.mapWorld (\(Velocity v :& Position x) -> Position (x + v)) w in w'
+run :: World -> IO ()
+run w = do
+  let s =
+        const ()
+          <$> S.map
+            ( proc () -> do
+                Velocity v <- Q.fetch -< ()
+                Position p <- Q.fetch -< ()
+                Q.set -< Position $ p + v
+            )
+  !_ <- S.runSystemWithWorld s w
+  return ()
 
 main :: IO ()
 main = do
   let !w =
         foldr
           ( \_ wAcc ->
-              let (e, wAcc') = W.spawn (Position 0) wAcc
+              let (e, wAcc') = W.spawn (bundle $ Position 0) wAcc
                in W.insert e (Velocity 1) wAcc'
           )
           W.empty
           [0 :: Int .. 10000]
-  defaultMain [bench "iter" $ whnf run w]
+  defaultMain [bench "iter" $ nfIO (run w)]
