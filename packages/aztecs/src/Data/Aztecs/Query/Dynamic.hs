babb5ab8b6b5e35779a9d7f008b3b44ccee8b4a4
Matt Hunzinger <matthunz2@gmail.com>
2025-02-18 16:09:24 -0500

    feat!: pure queries

--- packages/aztecs/src/Data/Aztecs/Query/Dynamic.hs
+++ packages/aztecs/src/Data/Aztecs/Query/Dynamic.hs
@@ -38,54 +38,56 @@ instance Monoid DynamicQueryFilter where
   mempty = DynamicQueryFilter mempty mempty
 
 -- | Dynamic query for components by ID.
-newtype DynamicQuery m i o = DynamicQuery
-  { dynQueryAll :: [i] -> [EntityID] -> Archetype -> m ([o], Archetype)
+newtype DynamicQuery i o = DynamicQuery
+  { dynQueryAll :: [i] -> [EntityID] -> Archetype -> ([o], Archetype)
   }
 
-instance (Functor m) => Functor (DynamicQuery m i) where
+instance Functor (DynamicQuery i) where
   fmap f q =
-    DynamicQuery $ \i es arch -> fmap (\(a, arch') -> (fmap f a, arch')) $ dynQueryAll q i es arch
+    DynamicQuery $ \i es arch ->
+      let (a, arch') = dynQueryAll q i es arch
+       in (fmap f a, arch')
 
-instance (Monad m) => Applicative (DynamicQuery m i) where
-  pure a = DynamicQuery $ \_ es arch -> pure (take (length es) $ repeat a, arch)
+instance Applicative (DynamicQuery i) where
+  pure a = DynamicQuery $ \_ es arch -> (take (length es) $ repeat a, arch)
 
   f <*> g =
     DynamicQuery
-      { dynQueryAll = \i es arch -> do
-          (as, arch') <- dynQueryAll g i es arch
-          (fs, arch'') <- dynQueryAll f i es arch'
-          return (zipWith ($) fs as, arch'')
+      { dynQueryAll = \i es arch ->
+          let (as, arch') = dynQueryAll g i es arch
+              (fs, arch'') = dynQueryAll f i es arch'
+           in (zipWith ($) fs as, arch'')
       }
 
-instance (Monad m) => Category (DynamicQuery m) where
-  id = DynamicQuery $ \as _ arch -> pure (as, arch)
+instance Category DynamicQuery where
+  id = DynamicQuery $ \as _ arch -> (as, arch)
 
   f . g =
     DynamicQuery
-      { dynQueryAll = \i es arch -> do
-          (as, arch') <- dynQueryAll g i es arch
-          dynQueryAll f as es arch'
+      { dynQueryAll = \i es arch ->
+          let (as, arch') = dynQueryAll g i es arch
+           in dynQueryAll f as es arch'
       }
 
-instance (Monad m) => Arrow (DynamicQuery m) where
-  arr f = DynamicQuery $ \bs _ arch -> pure (fmap f bs, arch)
+instance Arrow DynamicQuery where
+  arr f = DynamicQuery $ \bs _ arch -> (fmap f bs, arch)
   first f =
     DynamicQuery
-      { dynQueryAll = \bds es arch -> do
+      { dynQueryAll = \bds es arch ->
           let (bs, ds) = unzip bds
-          (cs, arch') <- dynQueryAll f bs es arch
-          return (zip cs ds, arch')
+              (cs, arch') = dynQueryAll f bs es arch
+           in (zip cs ds, arch')
       }
 
-instance (Monad m) => ArrowDynamicQueryReader (DynamicQuery m) where
-  entityDyn = DynamicQuery $ \_ es arch -> pure (es, arch)
+instance ArrowDynamicQueryReader DynamicQuery where
+  entityDyn = DynamicQuery $ \_ es arch -> (es, arch)
 
   fetchDyn cId =
-    DynamicQuery $ \_ _ arch -> let !as = A.all cId arch in pure (fmap snd as, arch)
+    DynamicQuery $ \_ _ arch -> let !as = A.all cId arch in (fmap snd as, arch)
 
   fetchMaybeDyn cId =
-    DynamicQuery $ \_ _ arch -> let as = A.allMaybe cId arch in pure (fmap snd as, arch)
+    DynamicQuery $ \_ _ arch -> let as = A.allMaybe cId arch in (fmap snd as, arch)
 
-instance (Monad m) => ArrowDynamicQuery (DynamicQuery m) where
+instance ArrowDynamicQuery DynamicQuery where
   setDyn cId =
-    DynamicQuery $ \is _ arch -> let !arch' = A.withAscList cId is arch in pure (is, arch')
+    DynamicQuery $ \is _ arch -> let !arch' = A.withAscList cId is arch in (is, arch')
--- packages/aztecs/src/Data/Aztecs/Query/Reader.hs
+++ packages/aztecs/src/Data/Aztecs/Query/Reader.hs
@@ -13,8 +13,6 @@ module Data.Aztecs.Query.Reader
     fetch,
     fetchMaybe,
     set,
-    task,
-    all,
 
     -- * Filters
     QueryFilter (..),
@@ -26,20 +24,14 @@ where
 
 import Control.Arrow (Arrow (..))
 import Control.Category (Category (..))
-import Control.Monad (mapM)
 import Data.Aztecs.Component
 import Data.Aztecs.Query.Class (ArrowQuery (..))
 import Data.Aztecs.Query.Dynamic (DynamicQueryFilter (..))
 import Data.Aztecs.Query.Dynamic.Reader (DynamicQueryReader (..))
 import Data.Aztecs.Query.Dynamic.Reader.Class (ArrowDynamicQueryReader (..))
 import Data.Aztecs.Query.Reader.Class (ArrowQueryReader (..))
-import Data.Aztecs.World (World (..))
-import qualified Data.Aztecs.World.Archetype as A
-import Data.Aztecs.World.Archetypes (Node (nodeArchetype))
-import qualified Data.Aztecs.World.Archetypes as AS
 import Data.Aztecs.World.Components (Components)
 import qualified Data.Aztecs.World.Components as CS
-import qualified Data.Map.Strict as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Prelude hiding (all, any, id, lookup, map, mapM, reads, (.))
@@ -60,75 +52,47 @@ import Prelude hiding (all, any, id, lookup, map, mapM, reads, (.))
 -- === Applicative combinators:
 -- > move :: (Monad m) => Query m () Position
 -- > move = (,) <$> Q.fetch <*> Q.fetch >>> arr (\(Position p, Velocity v) -> Position $ p + v) >>> Q.set
-newtype QueryReader m i o
-  = Query {runQueryReader :: Components -> (Set ComponentID, Components, DynamicQueryReader m i o)}
+newtype QueryReader  i o
+  = Query {runQueryReader :: Components -> (Set ComponentID, Components, DynamicQueryReader  i o)}
 
-instance (Functor m) => Functor (QueryReader m i) where
+instance  Functor (QueryReader  i) where
   fmap f (Query q) = Query $ \cs -> let (cIds, cs', qS) = q cs in (cIds, cs', fmap f qS)
 
-instance (Monad m) => Applicative (QueryReader m i) where
+instance Applicative (QueryReader i) where
   pure a = Query $ \cs -> (mempty, cs, pure a)
   (Query f) <*> (Query g) = Query $ \cs ->
     let (cIdsG, cs', aQS) = g cs
         (cIdsF, cs'', bQS) = f cs'
      in (cIdsG <> cIdsF, cs'', bQS <*> aQS)
 
-instance (Monad m) => Category (QueryReader m) where
+instance Category QueryReader  where
   id = Query $ \cs -> (mempty, cs, id)
   (Query f) . (Query g) = Query $ \cs ->
     let (cIdsG, cs', aQS) = g cs
         (cIdsF, cs'', bQS) = f cs'
      in (cIdsG <> cIdsF, cs'', bQS . aQS)
 
-instance (Monad m) => Arrow (QueryReader m) where
+instance  Arrow QueryReader  where
   arr f = Query $ \cs -> (mempty, cs, arr f)
   first (Query f) = Query $ \comps -> let (cIds, comps', qS) = f comps in (cIds, comps', first qS)
 
-instance (Monad m) => ArrowQueryReader (QueryReader m) where
+instance ArrowQueryReader QueryReader where
   entity = Query $ \cs -> (mempty, cs, entityDyn)
-  fetch :: forall a. (Component a) => QueryReader m () a
+  fetch :: forall a. (Component a) => QueryReader  () a
   fetch = Query $ \cs ->
     let (cId, cs') = CS.insert @a cs
      in (Set.singleton cId, cs', fetchDyn cId)
-  fetchMaybe :: forall a. (Component a) => QueryReader m () (Maybe a)
+  fetchMaybe :: forall a. (Component a) => QueryReader  () (Maybe a)
   fetchMaybe = Query $ \cs ->
     let (cId, cs') = CS.insert @a cs
      in (Set.singleton cId, cs', fetchMaybeDyn cId)
 
-instance (Monad m) => ArrowDynamicQueryReader (QueryReader m) where
+instance  ArrowDynamicQueryReader QueryReader where
   entityDyn = Query $ \cs -> (mempty, cs, entityDyn)
   fetchDyn cId = Query $ \cs -> (Set.singleton cId, cs, fetchDyn cId)
   fetchMaybeDyn cId = Query $ \cs -> (Set.singleton cId, cs, fetchMaybeDyn cId)
 
--- | Run a monadic task in a `Query`.
-task :: (Monad m) => (i -> m o) -> QueryReader m i o
-task f = Query $ \cs ->
-  ( mempty,
-    cs,
-    DynamicQueryReader $ \is _ _ -> mapM f is
-  )
 
--- | Query all matching entities.
---
--- >>> :set -XTypeApplications
--- >>> import Data.Aztecs
--- >>> import qualified Data.Aztecs.World as W
--- >>>
--- >>> newtype X = X Int deriving (Show)
--- >>> instance Component X
--- >>>
--- >>> let (_, w) = W.spawn (bundle $ X 0) W.empty
--- >>> (xs, _) <- all (fetch @_ @X) w
--- >>> xs
--- [X 0]
-all :: (Monad m) => QueryReader m () a -> World -> m ([a], World)
-all q w = do
-  let (rs, cs', dynQ) = runQueryReader q (components w)
-  as <-
-    mapM
-      (\n -> dynQueryReaderAll dynQ (repeat ()) (A.entities $ nodeArchetype n) (nodeArchetype n))
-      (Map.elems $ AS.lookup rs (archetypes w))
-  return (concat as, w {components = cs'})
 
 -- | Filter for a `Query`.
 newtype QueryFilter = QueryFilter {runQueryFilter :: Components -> (DynamicQueryFilter, Components)}
