64048517e5734151d171e7af61413d5d06911adb
Matt Hunzinger <matthunz2@gmail.com>
2025-03-03 17:25:44 -0500

    feat: system single query methods

--- src/Aztecs/ECS/System/Dynamic/Reader/Class.hs
+++ src/Aztecs/ECS/System/Dynamic/Reader/Class.hs
@@ -5,7 +5,23 @@ module Aztecs.ECS.System.Dynamic.Reader.Class (MonadDynamicReaderSystem (..)) wh
 import Aztecs.ECS.Component
 import Aztecs.ECS.World.Archetypes (Node)
 import Data.Set (Set)
+import GHC.Stack
 
 class (Monad m) => MonadDynamicReaderSystem q m | m -> q where
   allDyn :: i -> Set ComponentID -> q i o -> m [o]
+
+  singleDyn :: (HasCallStack) => i -> Set ComponentID -> q i o -> m o
+  singleDyn i cIds q = do
+    os <- allDyn i cIds q
+    case os of
+      [o] -> return o
+      _ -> error "singleDyn: expected a single result, but got multiple"
+
+  singleMaybeDyn :: i -> Set ComponentID -> q i o -> m (Maybe o)
+  singleMaybeDyn i cIds q = do
+    os <- allDyn i cIds q
+    return $ case os of
+      [o] -> Just o
+      _ -> Nothing
+
   filterDyn :: i -> Set ComponentID -> q i a -> (Node -> Bool) -> m [a]
--- src/Aztecs/ECS/System/Reader/Class.hs
+++ src/Aztecs/ECS/System/Reader/Class.hs
@@ -4,8 +4,24 @@
 module Aztecs.ECS.System.Reader.Class (MonadReaderSystem (..)) where
 
 import Aztecs.ECS.Query.Reader (QueryFilter (..))
+import GHC.Stack
 import Prelude hiding (all, any, filter, id, lookup, map, mapM, reads, (.))
 
 class (Monad m) => MonadReaderSystem q m | m -> q where
   all :: i -> q i o -> m [o]
+
+  single :: (HasCallStack) => i -> q i o -> m o
+  single i q = do
+    os <- all i q
+    case os of
+      [o] -> return o
+      _ -> error "single: expected a single result"
+
+  singleMaybe :: i -> q i o -> m (Maybe o)
+  singleMaybe i q = do
+    os <- all i q
+    return $ case os of
+      [o] -> Just o
+      _ -> Nothing
+
   filter :: i -> q i o -> QueryFilter -> m [o]
