ea112ec1322f73e9247f58cf61b3cb55a2e339a1
Matt Hunzinger <matthunz2@gmail.com>
2025-08-03 02:50:31 -0400

    doc: Aztecs.ECS.Class

--- src/Aztecs/ECS/Class.hs
+++ src/Aztecs/ECS/Class.hs
@@ -10,16 +10,29 @@ module Aztecs.ECS.Class (ECS (..)) where
 import Aztecs.ECS.HSet
 import Data.Kind
 
+-- | Entity Component System (ECS) implementation.
 class ECS m where
+  -- | Entity identifier.
   type Entity m :: Type
+
+  -- | Components that can be stored or accessed.
   type Components m :: [Type]
+
+  -- | Bundle of components that can be stored in an entity.
   type Bundle m :: Type
+
+  -- | Task monad for running systems.
   type Task m :: Type -> Type
 
+  -- | Spawn a new entity with a `Bundle` of components.
   spawn :: Bundle m -> m (Entity m)
 
+  -- | Insert a `Bundle` of components into an existing entity
+  -- (otherwise this will do nothing).
   insert :: Entity m -> Bundle m -> m ()
 
+  -- | Remove an entity and its components.
   remove :: Entity m -> m ()
 
+  -- | Run a `Task`.
   task :: (Task m) a -> m a
